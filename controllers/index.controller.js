15d270897dc699972fc45ed244e1fce430632325
Muhamad Yopan Ramadhan <yopanramadhan8@gmail.com>
2025-07-29 07:51:04 +0700

    delete: create product

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -1,6 +1,5 @@
 
 const { Product } = require('../models/product.model.js');
-const cloudinary = require('../config/cloudinary'); 
 
 const getDummyData = (req, res) => {
     return res.status(200).json({
@@ -9,33 +8,4 @@ const getDummyData = (req, res) => {
     });
 };
 
-const createProduct = async (req, res) => {
-    try {
-        if (!req.file) {
-            return res.status(400).json({ message: 'Gambar produk harus diunggah' });
-        }
-
-        const uploadStream = cloudinary.uploader.upload_stream({ resource_type: 'image' }, async (error, result) => {
-            if (error) {
-                console.error('Cloudinary Upload Error:', error);
-                return res.status(500).json({ message: 'Gagal mengunggah gambar ke Cloudinary' });
-            }
-
-            const newProduct = new Product({
-                ...req.body,
-                imageUrl: result.secure_url
-            });
-
-            await newProduct.save();
-            res.status(201).json({ message: 'Produk berhasil dibuat', data: newProduct });
-        });
-
-        uploadStream.end(req.file.buffer);
-
-    } catch (err) {
-        console.error('Server Error:', err);
-        res.status(500).json({ message: 'Terjadi kesalahan pada server', error: err.message });
-    }
-};
-
-module.exports = { getDummyData, createProduct };
+module.exports = { getDummyData };
--- routes/index.route.js
+++ routes/index.route.js
@@ -1,10 +1,8 @@
 const router = require('express').Router();
 const UserController = require("../controllers/index.controller.js")
-const { upload } = require('../config/cloudinary.js');
 
 
 router.get("/dummy", UserController.getDummyData)
-router.post('/create-product', upload.single('productImage'), UserController.createProduct);
 
 
 module.exports = router
